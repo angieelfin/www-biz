d720b2e8f10b2b84a06447075d2574c5ed90738e
Tomster <tomster@emberjs.com>
2017-04-07 16:07:39 +0200

    Vue setup

---
+++ src/components/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>WWW biz index</h1>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
