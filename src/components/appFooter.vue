f4fd9bbaaf56ab0c059869fc6b2dbd9f8e152326
Agata Kubiak <aga.kubia@gmail.com>
2017-04-14 20:08:40 +0200

    Add footer

---
+++ src/components/appFooter.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container padding-2/1-vertical">
+    <div class="social-media-bar">
+      <a href="https://twitter.com/angie_elfin" target="_blank">
+        <img src="../assets/footer-icons/twitter-logo.svg" alt="twitter logo">
+      </a>
+      <a href="https://github.com/angieelfin" target="_blank">
+        <img src="../assets/footer-icons/github-logo.svg" alt="github logo">
+      </a>
+      <a href="https://www.linkedin.com/in/agata-kubiak-029a13109/" target="_blank">
+        <img src="../assets/footer-icons/linkedin-logo.svg" alt="linkedin logo">
+      </a>
+      <a href="https://www.behance.net/AgataKubiak" target="_blank">
+        <img src="../assets/footer-icons/behance-logo.svg" alt="behance logo">
+      </a>
+      <a href="https://www.youtube.com/channel/UCQSAmGBbKkLsGAz0EULyVbA" target="_blank">
+        <img src="../assets/footer-icons/yt-logo.svg" alt="youtube logo">
+      </a>
+    </div>
+  </div>
+</template>
+
+
+<style scoped lang="scss">
+
+@import '~variables';
+
+.social-media-bar {
+  a img {
+    height: 35px;
+    padding: 0 2em;
+  }
+}
+
+
+</style>
